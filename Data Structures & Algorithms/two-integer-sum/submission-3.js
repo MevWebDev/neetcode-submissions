b293f6c7c611f9fd1df41b097c27411a90bee930
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-14 14:30:29 +0200

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map =  {};
+        for (let i = 0; i<nums.length;i++){
+            const rest = target - nums[i];
+            if(map[rest] != undefined) {
+                return [map[rest], i]
+            }
+            if(map[nums[i]] == undefined){
+                map[nums[i]] = i
+            }
+        }   
+   
+        return []
+    }
+
+}
