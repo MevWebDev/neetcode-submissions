c6019e9099028eedc98b7089da5ceba079421eff
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-15 13:20:35 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs) {
+    let encoded = "";
+    for (const str of strs) {
+      const length = str.length;
+      encoded += length + "#" + str;
+    }
+    return encoded
+  }
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {
+    const result = [];
+    let i = 0;
+    while (i < str.length) {
+      let j = i;
+      while (str[j] !== "#") j++;
+      const len = Number(str.slice(i, j));
+      const word = str.slice(j + 1, j + 1 + len);
+      result.push(word);
+      i = j + 1 + len;
+    }
+    return result;
+  }
+}
