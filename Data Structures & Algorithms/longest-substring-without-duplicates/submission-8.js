0ed2329f9740d39c751b2f0b725f11d0eab9bef4
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-15 16:46:32 +0200

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let longest = 0
+        if(s.length === 1) return 1
+        for (let i = 0; i < s.length; i++){
+            const set = new Set()
+            set.add(s[i])
+            for (let j = i +1; j < s.length; j++){
+                if(set.has(s[j])) {
+                    
+                    break;
+                }
+                
+                set.add(s[j])
+            }
+            longest = Math.max(longest,set.size)
+        }
+        return longest
+    }
+}
