6d3021cbdcbb1268f875d35100cad76959888d38
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-15 18:06:08 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        const s1Hash = s1.split('').sort().join('')
+        const s1L = s1.length;
+        for (let i = 0; i < s2.length; i++){
+            const sub = s2.slice(i,i+s1L).split('').sort().join('')
+            if(s1Hash === sub) return true
+        }
+        return false
+
+    }
+}
