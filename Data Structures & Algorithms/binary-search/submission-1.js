00a05ef5c858a4abcc2022e214bef59b9f59a01c
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-17 20:25:00 +0200

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        
+        let low = 0;
+        let high = nums.length - 1;
+
+        while (low <= high){
+            const middle = Math.floor((low + high) / 2)
+
+            if(nums[middle] === target) return middle
+
+            if(nums[middle] < target) {
+                low = middle + 1
+            }
+            else {
+                high = middle - 1
+            }
+        }
+
+        
+        return -1
+    }
+}
