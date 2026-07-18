4c4e38829c7c813a649d0904fa69471daed6a8c7
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-18 18:43:47 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = {};
+        for (let i = 0; i < nums.length; i++){
+            if (map[nums[i]] == undefined){
+                map[nums[i]] = 1;   
+            } 
+            else map[nums[i]] += 1;
+        }
+        const toSort = []
+        for(const entry in map){
+            toSort.push([entry, map[entry]])
+        }
+        const sorted = toSort.sort((a,b)=>b[1]-a[1])
+        const answer = [];
+        for (let i = 0 ; i < k; i++){
+            answer.push(sorted[i][0])
+        }
+        return answer
+    }
+}
