2c9ce48cca6a990c7436f5b5cefb9d6ac50300b3
Mev <58812843+MevWebDev@users.noreply.github.com>
2026-07-18 19:20:16 +0200

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let maxProd = 1;
+        let zeros = 0;
+        const output = new Array(nums.length)
+
+        nums.forEach((n)=>{
+            if(n === 0) zeros++;
+            else maxProd *= n;
+        })
+        if (zeros > 1) return new Array(nums.length).fill(0);
+        
+        else if (zeros === 1) {
+            const index = nums.indexOf(0);
+            nums.forEach((n,i)=>{
+                if(i!==index){
+                    output[i] = 0
+                } else {
+                    output[i] = maxProd
+                }
+            })
+        }
+
+        else {           
+            nums.forEach((n,i)=>{
+            output[i]= maxProd / n
+        })
+        }
+
+        return output;
+    }
+}
